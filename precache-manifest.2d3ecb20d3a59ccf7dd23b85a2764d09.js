c3f57909e91e1743ab241087ce04a46c236dc77f
vinsky <chunblo@gmail.com>
2022-02-25 12:45:28 +1100

    demo deploy

---
+++ precache-manifest.2d3ecb20d3a59ccf7dd23b85a2764d09.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae9ef7fbf2b85bb94a4f",
+    "url": "/css/app.f3466326.css"
+  },
+  {
+    "revision": "aeaf6399503956268d19",
+    "url": "/css/chunk-vendors.64d43e69.css"
+  },
+  {
+    "revision": "03a5d3281812144ee3c2fbbe3fb53c36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9218098a4fdf33e7bc14",
+    "url": "/js/about.323b62e5.js"
+  },
+  {
+    "revision": "ae9ef7fbf2b85bb94a4f",
+    "url": "/js/app.61704fd5.js"
+  },
+  {
+    "revision": "aeaf6399503956268d19",
+    "url": "/js/chunk-vendors.8b3e7527.js"
+  },
+  {
+    "revision": "56799944ce8bf55d452629ec830485b0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
